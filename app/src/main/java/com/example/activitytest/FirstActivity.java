c97ed6179d5ba7b740deea87ea619459befd4eff
wenyiYin <443644234@qq.com>
2023-06-08 16:26:50 +0800

    提交Toast，menu相关功能

--- app/src/main/java/com/example/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -4,15 +4,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
 
     private static final String TAG = "FirstActivity";
-    
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int itemId = item.getItemId();
+        if (itemId == R.id.add_item) {
+            Toast.makeText(FirstActivity.this, "you clicked add",
+                    Toast.LENGTH_SHORT).show();
+        } else if (itemId == R.id.remove_item) {
+            Toast.makeText(FirstActivity.this, "you clicked remove",
+                    Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
         Log.d(TAG, "onCreate: view for first_layout");
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Toast.makeText(FirstActivity.this,"you clicked button1",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
     }
 }
